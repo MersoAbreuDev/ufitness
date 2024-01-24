a84ea71d31915e288ee1df319f53826fb70d15d2
EmersonAEL <emerson@aelconsultoriasistemas.com.br>
2024-01-24 14:27:48 -0300

    logout

--- src/app/componentes/header/header.component.ts
+++ src/app/componentes/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, inject } from '@angular/core';
-import { FirebaseService } from 'src/services/firebase-services/firebase.service';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,7 +8,23 @@ import { FirebaseService } from 'src/services/firebase-services/firebase.service
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  items!:any[];
+  constructor(private afAuth: AngularFireAuth, private router: Router){
 
-  ngOnInit() {}
+  }
+
+  ngOnInit() {
+
+  }
+
+  async logout() {
+    console.log('teste');
+    try {
+      await this.afAuth.signOut();
+      this.router.navigate(['/auth']);
+    } catch (error) {
+      console.error('Erro ao fazer logout', error);
+    }
+  }
 
 }
